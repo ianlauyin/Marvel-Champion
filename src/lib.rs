03022eb0e72cc0f7f57713d0f826029e2be09355
ianlau <ianlauyin@gmail.com>
2024-11-08 12:45:17 +0800

    init

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,14 @@
 use bevy::prelude::*;
 
 #[bevy_main]
-fn start(){
-    App::new().run();
+pub fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, hello_world)
+        .run();
+}
+
+fn hello_world(mut commands: Commands) {
+    commands.spawn(Camera2d);
+    commands.spawn(Text::new("Hello World"));
 }
---
+++ src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    marvel_champions::main();
+}
