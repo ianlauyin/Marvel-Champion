4ca749cda51b2a2baf4cdd98cbb28af1873c4bda
ianlau <ianlauyin@gmail.com>
2024-11-11 10:41:42 +0800

    Update config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use bevy::prelude::*;
+
 fn main() {
-    marvel_champions::main();
+    App::new()
+    .add_plugins(DefaultPlugins)
+    .add_systems(Startup, hello_world)
+    .run();
+}
+
+
+fn hello_world(mut commands: Commands) {
+    commands.spawn(Camera2d);
+    commands.spawn(Text::new("Hello World"));
 }
