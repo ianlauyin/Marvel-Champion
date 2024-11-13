66e44ceacb45fcabb52f3a18f61025f8eb6465b2
ianlau <ianlauyin@gmail.com>
2024-11-13 18:41:02 +0800

    add spiderman hero card

--- src/features/cards/data/identity_specific_cards/core_spider_man.rs
+++ src/features/cards/data/identity_specific_cards/core_spider_man.rs
@@ -1,21 +1,21 @@
-use crate::features::cards::builders::*;
+use crate::features::cards::{builders::*, CardTrait};
 
 pub fn get_hero() -> HeroCard {
     HeroCard {
-        id: todo!(),
-        name: todo!(),
-        flip_target_id: todo!(),
-        initial_hit_points: todo!(),
-        keywords: todo!(),
-        traits: todo!(),
-        card_icons: todo!(),
-        thw: todo!(),
-        atk: todo!(),
-        def: todo!(),
-        description: todo!(),
-        search_keywords: todo!(),
-        hand_size: todo!(),
-        nemensis_id: todo!(),
-        image_path: todo!(),
+        id: String::from("core_1a"),
+        name: String::from("Spider-Man"),
+        flip_target_id: vec![String::from("core_1b")],
+        initial_hit_points: 10,
+        keywords: vec![],
+        traits: vec![CardTrait::Avenger],
+        card_icons: vec![],
+        thw: 1,
+        atk: 2,
+        def: 3,
+        description: String::from("Spider-Sense — Interrupt: When the villain initiates an attack against you, draw 1 card."),
+        search_keywords: vec![],
+        hand_size: 5,
+        nemensis_id: String::from("core_167"),
+        image_path: String::from("embedded://cards/identity_specific_cards/core_spider_man/core_1a.png"),
     }
 }
