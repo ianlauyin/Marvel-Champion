79a32e1a470560f1b52034439087c051f02f42fe
ianlau <ianlauyin@gmail.com>
2024-11-27 18:52:13 +0800

    Update list styling with scroll

--- src/features/shared/menu/list.rs
+++ src/features/shared/menu/list.rs
@@ -2,30 +2,53 @@ use bevy::prelude::*;
 
 use crate::features::shared::ButtonBuilder;
 
-use super::ListItem;
+use super::{ListItem, ScrollingList};
 
 pub fn spawn_list<B: Component>(menu: &mut ChildBuilder, button_map: Vec<(B, ListItem)>) {
+    // Scrolling List Container
     menu.spawn(NodeBundle {
         style: Style {
-            height: Val::Percent(80.),
-            width: Val::Percent(100.),
-            align_self: AlignSelf::Center,
-            display: Display::Grid,
-            grid_template_columns: vec![RepeatedGridTrack::auto(3)],
+            height: Val::Percent(90.),
+            align_self: AlignSelf::Stretch,
+            flex_direction: FlexDirection::Column,
+            overflow: Overflow::clip_y(),
             ..default()
         },
         ..default()
     })
-    .with_children(|container| {
-        for (button_component, button_info) in button_map {
-            let ListItem { text, color, image } = button_info;
-            let button = ButtonBuilder {
-                text,
-                color,
-                image,
-                ..default()
-            };
-            button.spawn(container).insert(button_component);
-        }
+    .with_children(|list_container| {
+        let row_gap = Val::Px(match button_map.len() {
+            0..10 => 80.,
+            10..13 => 50.,
+            _ => 30.,
+        });
+        // Moving Panel
+        list_container
+            .spawn((
+                ScrollingList::default(),
+                NodeBundle {
+                    style: Style {
+                        width: Val::Percent(100.),
+                        display: Display::Grid,
+                        padding: UiRect::all(Val::Px(30.)),
+                        grid_template_columns: vec![RepeatedGridTrack::auto(3)],
+                        row_gap,
+                        ..default()
+                    },
+                    ..default()
+                },
+            ))
+            .with_children(|container| {
+                for (button_component, button_info) in button_map {
+                    let ListItem { text, color, image } = button_info;
+                    let button = ButtonBuilder {
+                        text,
+                        color,
+                        image,
+                        ..default()
+                    };
+                    button.spawn(container).insert(button_component);
+                }
+            });
     });
 }
