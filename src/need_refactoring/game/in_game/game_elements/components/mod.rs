f0e1ed43cf5a6c4be91e8d482102e311e1496c1d
ianlau <ianlauyin@gmail.com>
2025-04-07 15:21:42 +0800

    remove useless folder

--- src/need_refactoring/game/in_game/game_elements/components/mod.rs
+++
@@ -1,9 +0,0 @@
-mod belongs;
-mod card_state;
-mod player;
-mod ui;
-
-pub use belongs::{Belongs, BelongsPlugin};
-pub use card_state::CardState;
-pub use player::Player;
-pub use ui::GameUiPlugin;
--- src/need_refactoring/game/in_game/game_elements/resources/mod.rs
+++
@@ -1,5 +0,0 @@
-mod first_player;
-mod player_number;
-
-pub use first_player::FirstPlayer;
-pub use player_number::PlayerNumber;
